20bda7622d1e7680c6f8c0e59be1bf4263d55bd5
electronic <nikitaftp99@gmail.com>
2023-07-20 12:16:26 +0500

    закончил адаптив слайдеров до 768

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const menuBtn = document.querySelector('.menu__btn');
+const menuMobile = document.querySelector('.header__menu-list');
+
+menuBtn.addEventListener('click', () => {
+  menuMobile.classList.toggle('menu--open');
+});
+
+const swiperOne = new Swiper('.feedback__slider', {
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
+
+const swiperTwo = new Swiper('.certificates__slider', {
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
